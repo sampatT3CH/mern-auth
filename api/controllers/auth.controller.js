49dc1f05e24f83fc6c89ff5a2d3afffaa06cd6c6
sampatT3CH <138952828+sampatT3CH@users.noreply.github.com>
2023-12-24 01:50:59 +0530

    signin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs"
+import { errorHandler } from "../utils/error.js";
+import jwt  from 'jsonwebtoken'
 
 export const signup = async (req,res,next) => {
     try {
@@ -17,4 +19,22 @@ export const signup = async (req,res,next) => {
      next(err)
     }
   
+}
+
+export const signin = async(req,res,next) => {
+    const {email, password} = req.body;
+    try{
+        
+      const validUser = await User.findOne({email})
+      if(!validUser) return next(errorHandler(404,"User Not Found"));
+      const validPassword = bcryptjs.compareSync(password, validUser.password);
+      if(!validPassword) return next(errorHandler(404,"Invalid Credential"));
+      const token = jwt.sign({id : validUser._id},process.env.JWT_SECRET);
+      const {password:hashedPassword,...rest} = validUser._doc
+      const expiryDate = new Date();
+      expiryDate.setTime(expiryDate.getTime() + 60 * 60 * 1000);
+      res.cookie('access_token',token, {httpOnly: true, expires : expiryDate}).status(200).json(rest)
+    }catch(err) {
+        next(err)
+    }
 }
