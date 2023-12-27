7b17c68cd92d01048aaf19144227ee1635f41c75
sampatT3CH <138952828+sampatT3CH@users.noreply.github.com>
2023-12-27 08:14:08 +0530

    oauth components

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -5,6 +5,10 @@ const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique:true },
     email:{type: String, required: true, unique:true},
     password: { type: String, required: true },
+    profilePicture: {
+        type: String,
+        default:"https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg"
+    }
 
 },{timestamps:true});
 
