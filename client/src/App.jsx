77ece3e53cdeff1ffb9e0cfca08e677da004d6c8
sampatT3CH <138952828+sampatT3CH@users.noreply.github.com>
2023-12-27 10:28:40 +0530

    update header componnt

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Home from './pages/Home'
 import Header from './components/Header'
+import PrivateRoute from './components/PrivateRoute'
 
 const App = () => {
   return (
@@ -15,7 +16,9 @@ const App = () => {
       <Route path="/sign-in" element={<SignIn />}></Route>
       <Route path="/sign-up" element={<SignUp />}></Route>
       <Route path="/about" element={<About />}></Route>
+      <Route element={<PrivateRoute />}>
       <Route path="/profile" element={<Profile />}></Route>
+      </Route>
   </Routes>
   </BrowserRouter>
   )
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 export default function Header() {
+  const {currentUser} = useSelector(state => state.user)
   return (
     <div className='bg-slate-200'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -12,7 +14,9 @@ export default function Header() {
             <ul className='flex gap-4'>
                 <Link to="/"><li>Home</li></Link>
                 <Link to="about"><li>About</li></Link>
-                <Link to="sign-in"><li>SignIn</li></Link>
+                <Link to="/profile">
+                {currentUser ? (<img src={currentUser.profilePicture} alt="picture" className='h-7 w-7 rounded-full object-cover' />) : (<li>SignIn</li>)}
+                </Link>
             </ul>
         </div>
     </div>
