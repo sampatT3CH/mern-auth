fdf81247ed8c6bda1c7ee98913a82555465125ba
sampatT3CH <138952828+sampatT3CH@users.noreply.github.com>
2024-01-02 11:56:20 +0530

    home and about page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
     <div className='bg-slate-200'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
             <Link to="/">
-            <h1 className='font-bold'>Auth App</h1>
+            <h1 className='font-bold'>Authentication App</h1>
             </Link>
             
             <ul className='flex gap-4'>
